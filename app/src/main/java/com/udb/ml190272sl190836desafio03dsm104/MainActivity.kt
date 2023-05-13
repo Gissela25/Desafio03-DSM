8c14eb45c115eecf91348c3abaff33167fcc8fa1
Gissela Serrano <55642333+Gissela25@users.noreply.github.com>
2023-05-12 20:27:35 -0600

    almost Done

--- app/src/main/java/com/udb/ml190272sl190836desafio03dsm104/MainActivity.kt
+++ app/src/main/java/com/udb/ml190272sl190836desafio03dsm104/MainActivity.kt
@@ -1,43 +1,50 @@
 package com.udb.ml190272sl190836desafio03dsm104
 
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.SearchView
+import android.widget.Toast
 import com.udb.ml190272sl190836desafio03dsm104.adapters.ListVerbsAdapter
 import com.udb.ml190272sl190836desafio03dsm104.db.DbVerbs
 import com.udb.ml190272sl190836desafio03dsm104.entidades.Verbs
-//import com.udb.testjava.adaptadores.ListaContactosAdapter
-//import com.udb.ml190272sl190836desafio03dsm104.db.DbVerbs
-//import com.udb.testjava.entidades.Contactos
+
 import java.util.ArrayList
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
+    private lateinit var txtBuscar: SearchView
     private lateinit var listVerbs: RecyclerView
     private lateinit var listArrayVerbs: ArrayList<Verbs>
+    private lateinit var adapter: ListVerbsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //Video 3
+        txtBuscar = findViewById(R.id.txtSearch)
         listVerbs = findViewById(R.id.listVerbs)
         listVerbs.layoutManager = LinearLayoutManager(this)
-
         val dbVerbs = DbVerbs(this)
 
         listArrayVerbs = ArrayList()
 
-       val adapter = ListVerbsAdapter(dbVerbs.showVerbs())
+        adapter = ListVerbsAdapter(dbVerbs.showVerbs())
         listVerbs.adapter = adapter
+
+        txtBuscar.setOnQueryTextListener(this)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_s, menu)
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_s, menu)
         return true
     }
 
@@ -56,5 +63,13 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    override fun onQueryTextSubmit(query: String): Boolean {
+        return false
+    }
+
+    override fun onQueryTextChange(s: String): Boolean {
+        adapter.filtrado(s)
+        return false
+    }
 }
 
--- app/src/main/java/com/udb/ml190272sl190836desafio03dsm104/adapters/ListVerbsAdapter.kt
+++ app/src/main/java/com/udb/ml190272sl190836desafio03dsm104/adapters/ListVerbsAdapter.kt
@@ -10,31 +10,52 @@ import androidx.recyclerview.widget.RecyclerView
 import com.udb.ml190272sl190836desafio03dsm104.R
 import com.udb.ml190272sl190836desafio03dsm104.ShowActivity
 import com.udb.ml190272sl190836desafio03dsm104.entidades.Verbs
-
 //import com.udb.testjava.R
+//import com.udb.testjava.VerActivity
 //import com.udb.testjava.entidades.Contactos
+import java.util.ArrayList
+import java.util.List
+import java.util.stream.Collectors
 
-class ListVerbsAdapter(private val listaVerbs: ArrayList<Verbs>) :
-    RecyclerView.Adapter<ListVerbsAdapter.ContactoViewHolder>() {
+class ListVerbsAdapter(private val listaVerbs: ArrayList<Verbs>) : RecyclerView.Adapter<ListVerbsAdapter.ContactoViewHolder>() {
+
+    // VideoS
+    private val listaOriginal: ArrayList<Verbs> = ArrayList(listaVerbs)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactoViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.list_item_verb, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_verb, parent, false)
         return ContactoViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContactoViewHolder, position: Int) {
         val verb = listaVerbs[position]
         holder.viewVerbo.text = verb.Verbo
- /*       holder.viewInd_Yo.text = verb.Ind_Yo
-        holder.viewInd_Tu.text = verb.Ind_Tu
-        holder.viewInd_El_Ella_Usted.text = verb.Ind_El_Ella_Usted
-        holder.viewInd_Nosotros.text = verb.Ind_Nosotros
-        holder.viewInd_Vosotros.text = verb.Ind_Vosotros
-        holder.viewInd_Ellos.text = verb.Ind_Ellos
-        holder.viewImp_Tu.text = verb.Imp_Tu
-        holder.viewImp_Nosotros.text = verb.Imp_Nosotros
-        holder.viewImp_Ellos_Ellas_Ustedes.text = verb.Imp_Ellos_Ellas_Ustedes*/
+        /*       holder.viewInd_Yo.text = verb.Ind_Yo
+               holder.viewInd_Tu.text = verb.Ind_Tu
+               holder.viewInd_El_Ella_Usted.text = verb.Ind_El_Ella_Usted
+               holder.viewInd_Nosotros.text = verb.Ind_Nosotros
+               holder.viewInd_Vosotros.text = verb.Ind_Vosotros
+               holder.viewInd_Ellos.text = verb.Ind_Ellos
+               holder.viewImp_Tu.text = verb.Imp_Tu
+               holder.viewImp_Nosotros.text = verb.Imp_Nosotros
+               holder.viewImp_Ellos_Ellas_Ustedes.text = verb.Imp_Ellos_Ellas_Ustedes*/
+    }
+
+    fun filtrado(txtBuscar: String) {
+        val longitud = txtBuscar.length
+        if (txtBuscar.length == 0) {
+            listaVerbs.clear()
+            listaVerbs.addAll(listaOriginal)
+        } else {
+            val collection: MutableList<Verbs>? = listaVerbs.stream()
+                .filter { i -> i.Verbo!!.toLowerCase().contains(txtBuscar.toLowerCase()) }
+                .collect(Collectors.toList())
+            listaVerbs.clear()
+            if (collection != null) {
+                listaVerbs.addAll(collection)
+            }
+        }
+        notifyDataSetChanged()
     }
 
     override fun getItemCount(): Int {
@@ -43,15 +64,15 @@ class ListVerbsAdapter(private val listaVerbs: ArrayList<Verbs>) :
 
     inner class ContactoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val viewVerbo: TextView = itemView.findViewById(R.id.viewVerbo)
-       /* val viewInd_Yo: TextView = itemView.findViewById(R.id.viewInd_Yo)
-        val viewInd_Tu: TextView = itemView.findViewById(R.id.viewInd_Tu)
-        val viewInd_El_Ella_Usted: TextView = itemView.findViewById(R.id.viewInd_El_Ella_Usted)
-        val viewInd_Nosotros: TextView = itemView.findViewById(R.id.viewInd_Nosotros)
-        val viewInd_Vosotros: TextView = itemView.findViewById(R.id.viewInd_Vosotros)
-        val viewInd_Ellos: TextView = itemView.findViewById(R.id.viewInd_Ellos)
-        val viewImp_Tu: TextView = itemView.findViewById(R.id.viewImp_Tu)
-        val viewImp_Nosotros: TextView = itemView.findViewById(R.id.viewImp_Nosotros)
-        val viewImp_Ellos_Ellas_Ustedes: TextView = itemView.findViewById(R.id.viewImp_Ellos_Ellas_Ustedes)*/
+        /* val viewInd_Yo: TextView = itemView.findViewById(R.id.viewInd_Yo)
+         val viewInd_Tu: TextView = itemView.findViewById(R.id.viewInd_Tu)
+         val viewInd_El_Ella_Usted: TextView = itemView.findViewById(R.id.viewInd_El_Ella_Usted)
+         val viewInd_Nosotros: TextView = itemView.findViewById(R.id.viewInd_Nosotros)
+         val viewInd_Vosotros: TextView = itemView.findViewById(R.id.viewInd_Vosotros)
+         val viewInd_Ellos: TextView = itemView.findViewById(R.id.viewInd_Ellos)
+         val viewImp_Tu: TextView = itemView.findViewById(R.id.viewImp_Tu)
+         val viewImp_Nosotros: TextView = itemView.findViewById(R.id.viewImp_Nosotros)
+         val viewImp_Ellos_Ellas_Ustedes: TextView = itemView.findViewById(R.id.viewImp_Ellos_Ellas_Ustedes)*/
 
         init {
             itemView.setOnClickListener {
